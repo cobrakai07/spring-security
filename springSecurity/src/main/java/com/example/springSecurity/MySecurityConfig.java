ed58ed3331e60722b351c4208b0d77798349a8ab
Kai <dk786161616@gmail.com>
2025-03-20 12:41:07 +0530

    added datasource

--- springSecurity/src/main/java/com/example/springSecurity/MySecurityConfig.java
+++ springSecurity/src/main/java/com/example/springSecurity/MySecurityConfig.java
@@ -1,15 +1,21 @@
 package com.example.springSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,13 +24,22 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class MySecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
         @Bean
         SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-            http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+            http.authorizeHttpRequests((requests) -> requests
+                    .requestMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated());
             //this will disable cookie and so on session
             http.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //            http.formLogin(withDefaults());
             http.httpBasic(withDefaults());
+
+            http.headers(headers-> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+            http.csrf(AbstractHttpConfigurer::disable);
+
             return http.build();
         }
 
@@ -39,6 +54,12 @@ public class MySecurityConfig {
                     .password("{noop}adminPassword1") //noop tells password should be saved as plain text to the spring boot
                     .roles("ADMIN")
                     .build();
-            return new InMemoryUserDetailsManager(user1,admin);
+
+            JdbcUserDetailsManager userDetailsManager= new JdbcUserDetailsManager(dataSource);
+            userDetailsManager.createUser(user1);
+            userDetailsManager.createUser(admin);
+            return userDetailsManager;
+
+            //return new InMemoryUserDetailsManager(user1,admin);
         }
 }
