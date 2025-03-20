bef10f45c9b4c6d190ef661454595aa8eaeebff9
Kai <dk786161616@gmail.com>
2025-03-21 00:19:38 +0530

    in memory security completed

--- springSecurity/src/main/java/com/example/springSecurity/MyTestController.java
+++ springSecurity/src/main/java/com/example/springSecurity/MyTestController.java
@@ -1,13 +1,34 @@
 package com.example.springSecurity;
 
+import com.example.springSecurity.dto.LoginRequest;
+import com.example.springSecurity.dto.LoginResponse;
+import com.example.springSecurity.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class MyTestController {
+
+    @Autowired
+    private JwtUtils utils;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/test")
     public String fun(){
         return "Testing endpoint...!";
@@ -23,4 +44,32 @@ public class MyTestController {
     public String admin(){
         return "Testing Admin endpoint...!";
     }
+
+
+
+    // jwt endpoints
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication;
+        try{
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        }catch(AuthenticationException ex){
+            Map<String, Object> map= new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status",false);
+            return new ResponseEntity<Object>(map, HttpStatus.NOT_FOUND);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails=(UserDetails) authentication.getPrincipal();
+
+        String jwtToken = utils.generateTokenFromUsername(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream().map(item-> item.getAuthority()).collect(Collectors.toList());
+
+        LoginResponse response = new LoginResponse(userDetails.getUsername(),roles, jwtToken);
+        return ResponseEntity.ok(response);
+    }
+
 }
---
+++ springSecurity/src/main/java/com/example/springSecurity/dto/LoginResponse.java
@@ -0,0 +1,39 @@
+package com.example.springSecurity.dto;
+
+import java.util.List;
+
+public class LoginResponse {
+    public LoginResponse(String username, List<String> roles, String jwtToken) {
+        this.username = username;
+        this.roles = roles;
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    private String username;
+    private List<String> roles;
+    private String jwtToken;
+}
